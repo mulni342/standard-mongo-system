e6d7538b8893b222f7ace5f711a6f6d2e80b6059
mulni <mulnicrafteroyt@gmail.com>
2021-11-30 22:18:21 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// import { BaseSystem } from '../../types/types';
+
+import * as discord from 'discord.js';
+
+export class System {
+    client: any;
+
+    constructor(client: any) {
+        if (client.debugLogs) console.log('system install created!');
+        this.client = client;
+    }
+
+    init() {
+        if (this.client.debugLogs) console.log('system was initialized!');
+    }
+
+    onReady() {
+        console.log('Bot Is online!');
+    }
+
+    loginToken(): string {
+        return 'OTA1OTI5NTM3MzI0NDQ5Nzky.YYROoQ.oG4Nj-BXxdJKexL2J-tP3hXzJFU';
+    }
+
+    onMessage(message: discord.Message) {
+        const prefix = process.env.PREFIX || 'ab!';
+
+        if (!message.content.startsWith(prefix)) return;
+
+        const command = message.content.split(' ')[0].slice(prefix.length);
+        const margs = message.content.split(' ').splice(1);
+
+        this.client.runCommand(command, message, margs);
+    }
+
+    commandArguments() {
+        return [];
+    }
+}
