9888215652a5b2c3241681103fb47d7d77eee981
mulni <mulnicrafteroyt@gmail.com>
2021-12-02 19:11:19 -0500

    bulld last commit

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,13 @@
 "use strict";
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // import { BaseSystem } from '../../types/types';
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.System = void 0;
+var setup_1 = require("./setup");
+var mongoose_1 = __importDefault(require("mongoose"));
 var System = /** @class */ (function () {
     function System(client) {
         if (client.debugLogs)
@@ -10,11 +15,20 @@ var System = /** @class */ (function () {
         this.client = client;
     }
     System.prototype.init = function () {
+        var _this = this;
         if (this.client.debugLogs)
             console.log('system was initialized!');
+        if (!process.env.URI)
+            throw Error('Database URI not define in enviroment variables');
+        mongoose_1.default.connect(process.env.URI, function () {
+            if (_this.client.debugLogs) {
+                console.log('Database connected!');
+            }
+        });
     };
     System.prototype.onReady = function () {
         console.log('Bot Is online!');
+        (0, setup_1.checkConfigDocument)(this.client);
     };
     System.prototype.loginToken = function () {
         return process.env.TOKEN;
---
+++ dist/setup/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.checkConfigDocument = void 0;
+function checkConfigDocument(client) {
+}
+exports.checkConfigDocument = checkConfigDocument;
